f48591341cd92db40630339f4a180fdc8b14b300
minhaz-hossain02 <mhminhaz02@gmail.com>
2025-08-15 10:16:20 -1200

    Added Multiply two floating number

---
+++ Multiply_two_floating_number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+float num1, num2, result;
+
+cout<<"Enter two number:";
+cin>>num1>>num2;
+
+result=num1*num2;
+
+cout<<"The product is:"<<result<<endl;
+
+return 0;
+
+}
+
+
+
