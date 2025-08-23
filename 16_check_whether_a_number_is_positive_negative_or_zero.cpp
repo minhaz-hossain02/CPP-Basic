f3e3196f6bad9854d63281234bb4513510131c52
minhaz-hossain02 <mhminhaz02@gmail.com>
2025-08-22 22:56:20 -1200

    added check whether a number is positive, negative or zero

---
+++ 16_check_whether_a_number_is_positive_negative_or_zero.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+int number;
+
+cout<<"Enter the number: ";
+cin>>number;
+
+if (number>0)
+    cout<<"positive number";
+else if (number<0)
+    cout<<"negative number";
+else
+    cout<<"zero";
+
+    return 0;
+
+}
