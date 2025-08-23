90300776f11d9c1ca48d81b0e55d4eade56e5967
minhaz-hossain02 <mhminhaz02@gmail.com>
2025-08-23 03:14:33 -1200

    added check leap year

---
+++ 22_check_leap_year.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+int year;
+
+cout<<"Enter a year: ";
+cin>>year;
+
+if (year % 400 == 0){
+    cout<<"Leap year: " <<year;
+}
+else if (year % 4 ==0 && year % 100 != 0){
+    cout<<"Leap year: " << year;
+}
+else{
+    cout<<"Not a leap year:" <<year;
+}
+return 0;
+}
