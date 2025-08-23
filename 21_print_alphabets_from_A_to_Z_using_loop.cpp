c0a690aa9658528e2bffd9d8abbfcf8f917d2341
minhaz-hossain02 <mhminhaz02@gmail.com>
2025-08-23 02:54:48 -1200

    added for(ch = 'A'; ch <= 'Z'; ch++) {

---
+++ 21_print_alphabets_from_A_to_Z_using_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+char ch;
+
+cout<<"Alphabet from A to Z are: " << endl;
+for(ch = 'A'; ch <= 'Z'; ch++) {
+        cout << ch << " ";
+}
+return 0;
+
+}
