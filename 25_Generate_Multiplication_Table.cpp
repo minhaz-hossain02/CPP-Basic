5e30eee34c7965c452457ff4711fba7d0842f89e
minhaz-hossain02 <mhminhaz02@gmail.com>
2025-11-13 06:55:37 -1200

    25_Generate_Multiplication_Table

---
+++ 25_Generate_Multiplication_Table.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, i;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "\nMultiplication Table of " << num << ":\n";
+
+    for (i = 1; i <= 10; i++) {
+        cout << num << " x " << i << " = " << num * i << endl;
+    }
+
+    return 0;
+}
+
