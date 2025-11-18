d5b4441df374fa9e54bfd18bfde199113e40d4ea
minhaz-hossain02 <mhminhaz02@gmail.com>
2025-11-18 08:21:59 -1200

    46_inverted_hollow_star_pyramid

--- 40_1800_rotation_of_simple_pyramid.cpp
+++ 40_1800_rotation_of_simple_pyramid.cpp
@@ -8,17 +8,15 @@ int main() {
     cin >> n;
 
     for (int i = 1; i <= n; i++) {
-        // print spaces
-        for (int j = 1; j <= n - i; j++) {
-            cout << "  ";
-        }
-        // print stars
-        for (int k = 1; k <= i; k++) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
+    for (int j = 1; j <= n - i; j++) {
 
+    cout << "  ";
+    }
+    for (int k = 1; k <= i; k++) {
+    cout << "* ";
+    }
+    cout << endl;
+    }
     return 0;
 }
 
---
+++ 46_inverted_hollow_star_pyramid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = n; i >= 1; i--) {
+    for (int s = 1; s <= n - i; s++) {
+    cout << " ";
+   }
+    for (int j = 1; j <= (2 * i - 1); j++) {
+    if (i == n || j == 1 || j == (2 * i - 1))
+    cout << "*";
+    else
+    cout << " ";
+    }
+    cout << endl;
+    }
+    return 0;
+}
+
