6cb24f0cae40219a966fbdb4a0182b55ad571693
minhaz-hossain02 <mhminhaz02@gmail.com>
2025-11-26 18:46:06 -1200

    61_print_a_2D_array

---
+++ 61_print_a_2D_array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    int arr[100][100];
+
+
+    cout << "Enter the elements of the 2D array:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+
+    cout << "\nThe 2D array is:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
